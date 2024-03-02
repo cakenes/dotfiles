8557d1a2337514f511911f8932c7f8203cbd4ff7
cakenes <jyri.kurtakko@gmail.com>
2024-03-02 09:55:36 +0200

    Bring over few configs from mac

---
+++ .config/nvim/lua/plugins/git-blame.lua
@@ -0,0 +1,6 @@
+return {
+    {
+        "f-person/git-blame.nvim",
+        name = "git-blame",
+    },
+}
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -11,11 +11,19 @@ return {
             },
         },
         dependencies = {
-            "nvim-telescope/telescope-fzf-native.nvim",
-            build = "make",
-            config = function()
-                require("telescope").load_extension("fzf")
-            end,
+            {
+                "nvim-telescope/telescope-live-grep-args.nvim",
+                config = function()
+                    require("telescope").load_extension("live_grep_args")
+                end,
+            },
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
+                config = function()
+                    require("telescope").load_extension("fzf")
+                end,
+            },
         },
         opts = {
             defaults = {
