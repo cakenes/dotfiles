803465983e77413ed8bb97ac8126dce5b5698368
Jyri <jyri.kurtakko@pihlajalinna.fi>
2025-02-06 16:48:42 +0200

    Hint and warning as grey & cleanup cloak import

--- .config/nvim/lua/plugins/cloak.lua
+++ .config/nvim/lua/plugins/cloak.lua
@@ -1,20 +1,19 @@
 return {
-    {
-        "laytan/cloak.nvim",
-        enabled = true,
-        opts = function(_, opts)
-            opts.cloak_character = "*"
-            opts.highlight_group = "Comment"
-            opts.patterns = {
-                {
-                    file_pattern = {
-                        ".env*",
-                        "wrangler.toml",
-                        ".dev.vars",
-                    },
-                    cloak_pattern = "=.+",
+    "laytan/cloak.nvim",
+    enabled = true,
+    opts = function(_, opts)
+        opts.cloak_character = "*"
+        opts.highlight_group = "Comment"
+        opts.patterns = {
+            {
+                file_pattern = {
+                    ".env*",
+                    "wrangler.toml",
+                    ".dev.vars",
                 },
-            }
-        end,
-    },
+                cloak_pattern = "=.+",
+            },
+        }
+    end,
 }
+
