b7da5f5381fccc5b734112eb65d4b21cf2563203
Jyri <jyri.kurtakko@pihlajalinna.fi>
2025-02-06 16:48:42 +0200

    Hint and warning as grey & cleanup cloak import

--- .config/nvim/lua/plugins/vscode.lua
+++ .config/nvim/lua/plugins/vscode.lua
@@ -6,5 +6,13 @@ return {
             disable_nvimtree_bg = true,
         })
         vim.cmd.colorscheme("vscode")
+
+        local c = require("vscode.colors").get_colors()
+
+        -- vim.api.nvim_set_hl(0, "@type", { fg = c.vscLightBlue, bg = "NONE" }) -- Global typing
+        vim.api.nvim_set_hl(0, "DiagnosticInfo", { fg = c.vscLineNumber, bg = "NONE" })
+        vim.api.nvim_set_hl(0, "DiagnosticHint", { fg = c.vscLineNumber, bg = "NONE" })
+        vim.api.nvim_set_hl(0, "DiagnosticUnderLineInfo", { fg = c.vscLineNumber, bg = "NONE" })
+        vim.api.nvim_set_hl(0, "DiagnosticUnderLineHint", { fg = c.vscLineNuumber, bg = "NONE" })
     end,
 }
