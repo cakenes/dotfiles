2b2b2a4301990f31608232a47316ade56b5778be
cakenes <jyri.kurtakko@gmail.com>
2025-09-12 09:04:48 +0300

    auto open neotree and disable persistance

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -26,14 +26,3 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-
--- Restore session
-vim.api.nvim_create_autocmd("VimEnter", {
-    group = vim.api.nvim_create_augroup("restore_session", { clear = true }),
-    callback = function()
-        if vim.fn.getcwd() ~= vim.env.HOME then
-            require("persistence").load()
-        end
-    end,
-    nested = true,
-})
--- .config/nvim/lua/plugins/persistence.lua
+++
@@ -1,5 +0,0 @@
-return {
-    "folke/persistence.nvim",
-    event = "BufReadPre",
-    opts = { options = vim.opt.sessionoptions:get() },
-}
