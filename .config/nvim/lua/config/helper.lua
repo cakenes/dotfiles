6cd06b7d739257f072d083fe5b8312a9a639c38d
cakenes <jyri.kurtakko@gmail.com>
2025-09-12 08:52:02 +0300

    add cached grep

--- .config/nvim/lua/config/helper.lua
+++ .config/nvim/lua/config/helper.lua
@@ -5,3 +5,14 @@ function Copilot_chat(command)
     end
 end
 
+function Grep_cached_files()
+    local builtin = require("telescope.builtin")
+    local files = vim.fn.systemlist("git ls-files --cached")
+
+    if #files == 0 then
+        vim.notify("No cached files found in Git", vim.log.levels.WARN)
+        return
+    end
+
+    builtin.grep_string({ search_dirs = files })
+end
