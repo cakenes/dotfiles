6f17477a369d24fadcb6591494b36b11ef0cad88
cakenes <jyri.kurtakko@gmail.com>
2025-12-08 08:17:33 +0200

    Faster updatetime

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -70,7 +70,10 @@ return {
                 },
             },
             auto_install = true,
-            highlight = { enable = true },
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
             indent = { enable = true },
         })
     end,
