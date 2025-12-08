1c1ba79fd9d5eabafe1a00ba0410a1ace52037f2
cakenes <jyri.kurtakko@gmail.com>
2025-12-08 19:03:50 +0200

    Remove nvim folder

--- .config/nvim/lua/plugins/sleuth.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "tpope/vim-sleuth",
-    config = function()
-        vim.g.sleuth_automatic = 1
-    end,
-}
--- .config/nvim/lua/plugins/trouble.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "folke/trouble.nvim",
-    dependencies = {
-        "kyazdani42/nvim-web-devicons",
-    },
-    cmd = { "TroubleToggle", "Trouble" },
-    opts = { use_diagnostic_signs = true },
-}
