bfcfb588e8507325eb3a30e8b495fba57700c6a6
cakenes <jyri.kurtakko@gmail.com>
2025-12-08 19:26:59 +0200

    Remove nvim folder

--- .config/nvim/lua/plugins/copilot.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "github/copilot.vim",
-    config = function()
-        vim.g.copilot_no_tab_map = true
-    end,
-}
-
--- .config/nvim/lua/plugins/gitblame.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "f-person/git-blame.nvim",
-        opts = function(_, opts)
-            opts.delay = 0
-            vim.g.gitblame_highlight_group = "NonText"
-        end,
-    },
-}
